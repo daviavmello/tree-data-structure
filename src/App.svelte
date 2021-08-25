5130270b83a4c59e109cc421dcef2b9001814d9f
Davi Mello <davimello1994@gmail.com>
2021-08-25 11:04:05 -0400

    svelte ts tailwind bootstrap

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import Tailwind from "./Tailwind.svelte"
+</script>
+
+<style lang="scss">
+    main {
+        h1 {
+            font-size: 22rem;
+        }
+    }
+</style>
+
+<Tailwind />
+<main>
+    <h1 class="text-blue-900">Hello!</h1>
+    <p>
+        Visit the
+        <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+        to learn how to build Svelte apps.
+    </p>
+</main>
