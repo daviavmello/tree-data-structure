22abba6cdfc074ee94a3057db0f39195a75ed0a4
Davi Mello <davimello1994@gmail.com>
2021-08-26 23:03:29 -0400

    recursive folder call

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,29 +1,44 @@
 <script lang="ts">
     import Tailwind from "./Tailwind.svelte";
+    // import { onMount } from "svelte";
+    import axios from "axios"
     
     let folderName = "";
-    console.log(folderName);
+    let directories = null;
+	let submitted = false;
     
-</script>
-
-<style lang="scss">
-    main {
-        h1 {
-            font-size: 3rem;
+    async function onFolderChange () {
+        if (folderName.length > 0) {
+            submitted = true
+            const res = await axios.get(`http://localhost:8080/`, {params:{ name: folderName}})
+            
+            const data = await res.data;
+            console.log(folderName);
+            directories = data;
+            console.log(data);
         }
     }
-</style>
+</script>
 
 <Tailwind />
 <main>
     <div class="container mx-auto px-8">
-    <h1 class="text-blue-600">Tree data structure</h1>
-    <form class="flex">
-        <input placeholder="Enter a local directory name" class="w-5/6" bind:value={folderName}/>
-        <button class="w-1/6"></button>
-    </form>
+        <h1 class="text-blue-600">Tree data structure</h1>
+            <!-- <form class="flex" on:submit|preventDefault={onFolderChange} > -->
+                <input required placeholder="Enter a local directory name" class="w-full" bind:value={folderName} on:change|preventDefault={onFolderChange}/>
+                <!-- <button class="w-1/6" on:click={() => submitted = true}>Send</button> -->
+            <!-- </form> -->
     {#if folderName}
     <h2 class="text-blue-600">Searching results for {folderName}</h2>
     {/if}
-</div>
+    </div>
 </main>
+
+<!-- <style lang="scss">
+    main {
+        width: 100vw;
+        h1 {
+            font-size: 3rem;
+        }
+    }
+</style> -->
--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,46 @@
 import express from "express";
+// import * as fs from "fs";
+import path from "path";
+import cors from "cors";
+import fs from "fs";
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 const app = express();
+
+app.use(cors());
+
+interface IFolder {
+  path: string;
+  children: Array<IFolder>;
+}
+
+app.get("/", (req, res) => {
+  const rootPath = (req.query.name as string).trim();
+  console.log(`input: ${req.query.name}`);
+
+  let depth = 0;
+
+  const _getAllFilesFromFolder = (dir: string) => {
+    const obj: IFolder = { path: "", children: [] };
+    console.log(obj);
+    obj.path = dir;
+    console.log(`Dir: ${dir}`); //     /Users/davimello/Desktop/Reginaldo
+
+    const stats = fs.statSync(dir);
+    if (stats && stats.isDirectory()) {
+      console.log(`Dir: ${dir}`);
+
+      fs.readdirSync(dir).forEach((file) => {
+        const filePath = dir + "/" + file;
+
+        obj.children.push(_getAllFilesFromFolder(filePath));
+      });
+    }
+    return obj;
+  };
+
+  res.send(_getAllFilesFromFolder(rootPath as string));
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
